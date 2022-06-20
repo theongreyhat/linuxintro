ef55bcee6b2a3641ad5262f2324ebe5e30cb3d0c
theongreyhat <107890871+theongreyhat@users.noreply.github.com>
2022-06-20 16:46:02 -0500

    Create vscodeInstall.sh

---
+++ vscodeInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#vscode
+sudo apt install software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update -y
+sudo apt install code
